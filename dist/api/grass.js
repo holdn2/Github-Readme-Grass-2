8914d59f42cd837b909ace69c26057d8f272dfda
holdn2 <yoocy01@gmail.com>
2026-05-12 19:30:17 +0900

    Remove generated files from repository

--- dist/api/grass.js
+++
@@ -1,6 +0,0 @@
-import { makeGardenResponse } from "../src/handler.js";
-export default async function handler(request, response) {
-    const result = await makeGardenResponse(request.query ?? {});
-    Object.entries(result.headers).forEach(([key, value]) => response.setHeader(key, value));
-    response.status(result.status).send(result.body);
-}
